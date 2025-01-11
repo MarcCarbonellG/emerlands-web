db03f6347c4d4128f3e725c350bbdd9320db685d
MarcCarbonellUOC <marccarbonell@uoc.edu>
2025-01-11 17:02:12 +0000

    About Us

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
   setupDropdownToggle();
   setupHeaderScroll();
 });
+
+const cards = document.querySelectorAll('.card');
+
+// Handles cards fade in animation
+const toggleCardsOnScroll = () => {
+  cards.forEach((card) => {
+    const cardTop = card.getBoundingClientRect().top;
+    const cardBottom = card.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+
+    if (cardTop < windowHeight - 100 && cardBottom > 100) {
+      card.classList.add('show');
+    } else {
+      card.classList.remove('show');
+    }
+  });
+};
+
+window.addEventListener('scroll', toggleCardsOnScroll);
+window.addEventListener('load', toggleCardsOnScroll);
